d7de7b6d05d7c0462e61a8c69f47f311b0697b20
Ignacio F. Garcés <48460500+ifgarces@users.noreply.github.com>
2025-03-19 17:41:40 -0300

    Added AntDesign as UI library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <AntdRegistry>
+          {children}
+        </AntdRegistry>
       </body>
     </html>
   );
