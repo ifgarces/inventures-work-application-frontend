8eaa89c4dbe9d431cbe1f22353ad0ebe24dd49b4
Ignacio F. Garcés <48460500+ifgarces@users.noreply.github.com>
2025-03-19 17:34:13 -0300

    Added Axios, lodash and other utility libraries, updated pnpm scripts, applied personal ESLint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,7 @@ export default function Home() {
           alt="Next.js logo"
           width={180}
           height={38}
-          priority
-        />
+          priority/>
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
@@ -25,23 +24,20 @@ export default function Home() {
             className={styles.primary}
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <Image
               className={styles.logo}
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
-              height={20}
-            />
+              height={20}/>
             Deploy now
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles.secondary}
-          >
+            className={styles.secondary}>
             Read our docs
           </a>
         </div>
@@ -50,43 +46,37 @@ export default function Home() {
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <Image
             aria-hidden
             src="/file.svg"
             alt="File icon"
             width={16}
-            height={16}
-          />
+            height={16}/>
           Learn
         </a>
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <Image
             aria-hidden
             src="/window.svg"
             alt="Window icon"
             width={16}
-            height={16}
-          />
+            height={16}/>
           Examples
         </a>
         <a
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <Image
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
             width={16}
-            height={16}
-          />
+            height={16}/>
           Go to nextjs.org →
         </a>
       </footer>
