93f7930779831216255364455af3af8050f198d9
Ignacio F. Garcés <48460500+ifgarces@users.noreply.github.com>
2025-03-21 22:39:08 -0300

    Fixed strange issue regarding `TypeError` for production runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import "@ant-design/v5-patch-for-react-19";
+"use client"; //? this seems to somehow fix `Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')` from `share-modal.js:1:135`
+
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "@ant-design/v5-patch-for-react-19";
 import "./globals.css";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
